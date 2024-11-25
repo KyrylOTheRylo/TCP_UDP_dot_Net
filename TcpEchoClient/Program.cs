dfb31b4124ee39d05cd5d7bf17c33c662a8432b3
KyryloTheRylo <kiril.mosiychuk@gmail.com>
2024-11-26 00:11:11 +0200

    Implement listener for tcp protocol and client.

---
+++ TcpEchoClient/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+
+
+class TcpEchoClient
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 3 && args.Length !=2)
+        {
+            throw new ArgumentException("Parameters <Server> <word> [<Port>]");
+        }
+
+        String server = args[0];
+        Console.WriteLine(server);
+        byte[] byteBuffer = Encoding.ASCII.GetBytes(args[1]);
+        int servPort = (args.Length == 3) ? Int32.Parse(args[2]) : 7;
+
+        TcpClient client = null;
+        NetworkStream stream = null;
+
+        try
+        {
+            client = new TcpClient(server, servPort);
+            Console.WriteLine("Connected to server .... Sending echo string");
+
+            stream = client.GetStream();
+
+            stream.Write(byteBuffer, 0, byteBuffer.Length);
+
+            Console.WriteLine("Sent {0} bytes to server...", byteBuffer.Length);
+
+            int totalByteRcvd = 0;
+            int bytesRcvd = 0;
+
+            while ( totalByteRcvd < byteBuffer.Length)
+            {
+                if ((bytesRcvd = stream.Read(byteBuffer, totalByteRcvd, byteBuffer.Length - totalByteRcvd)) == 0)
+                {
+                    Console.WriteLine("Connection closed prematurely.");
+                    break;
+                }
+                totalByteRcvd += bytesRcvd;
+
+            }
+            Console.WriteLine("Received {0} bytes from server: {1}", totalByteRcvd, Encoding.ASCII.GetString(byteBuffer, 0, totalByteRcvd));
+
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+
+        }
+        finally
+        {
+            stream.Close();
+            client.Close();
+
+        }
+    }
+}
+
---
+++ TspServer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class TcpServer
+    
+{
+    private const int BUFFSIZE = 32;
+    static void Main(string[] args)
+    {
+        //byte[] bytes = Encoding.ASCII.GetBytes("server.exampe.com");
+        //IPAddress address = new IPAddress(bytes);
+        int port = 7;
+        TcpListener server = new TcpListener(IPAddress.Any, port);
+        server.Start();
+        byte[] receiveBuffer = new byte[BUFFSIZE];
+        int bytesRcvd = 0;
+        while (true)
+        {
+            Console.WriteLine("Working");
+            TcpClient connection = null;
+            NetworkStream stream = null;
+            try
+            {
+                connection = server.AcceptTcpClient();
+                stream= connection.GetStream();
+                Console.WriteLine("Handling stream {0}", connection);
+                int totalBytesEchoed = 0;
+                while ((bytesRcvd = stream.Read(receiveBuffer, 0, receiveBuffer.Length)) > 0) {
+                    stream.Write(receiveBuffer, 0, bytesRcvd);
+                    totalBytesEchoed += bytesRcvd;
+                }
+                Console.WriteLine("Bytes echoed {0}", totalBytesEchoed);
+
+                stream.Close();
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0}", e);
+                stream.Close();
+                
+            }
+
+
+        }
+
+    }
+}
+
